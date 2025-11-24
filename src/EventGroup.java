1aa38b3cbb71425247145bcf9068c2abe2bb4365
Asterialumi <asterialumi@gmail.com>
2025-11-24 21:50:21 +0800

    updated EventGroup

--- src/EventGroup.java
+++ src/EventGroup.java
@@ -1,4 +1,10 @@
+import java.util.List;
+
 public class EventGroup {
+    private String name;
+    private int numOfEvents;
+    private List<Event> listOfEvents;
+
     public EventGroup(){
 
     }
