7105129c33af12663f10c707f39c6c91e57b34a8
Asterialumi <asterialumi@gmail.com>
2025-11-25 14:04:37 +0800

    completed implementation of Event, needs review

--- src/Event.java
+++ src/Event.java
@@ -9,23 +9,57 @@ public class Event {
     private String pathName;
 
     public Event(){
-
+        //will remove this once Event class is properly constructed in other classes
     }
 
-
-    public String getName() {
-        return name;
+    public Event(String name, EventGroup eventGroup, String lateTime, String pathName) {
+        this.name = name;
+        this.eventGroup = eventGroup;
+        this.lateTime = lateTime;
+        this.pathName = pathName;
+        this.numOfAttendees = 0;
+        this.listOfAttendees = new ArrayList<>();
     }
 
-    public int getNumOfAttendees() {
-        return numOfAttendees;
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+
+    public EventGroup getEventGroup() { return eventGroup; }
+    public void setEventGroup(EventGroup eventGroup) { this.eventGroup = eventGroup; }
+
+
+    public String getLateTime() { return lateTime; }
+    public void setLateTime(String lateTime) { this.lateTime = lateTime; }
+
+
+    public List<Person> getListOfAttendees() { return listOfAttendees; }
+    public int getNumOfAttendees() { return numOfAttendees; }
+
+
+    public void addAttendee(Person p) {
+        listOfAttendees.add(p);
+        numOfAttendees++;
     }
 
-    public List<Person> getListOfAttendees() {
-        return listOfAttendees;
+    public void removeAttendee(Person p) {
+        listOfAttendees.remove(p);
+        numOfAttendees--;
     }
 
     public String getPathName() {
         return pathName;
+    }
+
+    public String toString() {
+        //not sure if this was the intended use of toString
+        //haven't included listOfAttendees
+        String next = System.lineSeparator();
+        String eventName = "Event: " + name;
+        String eventGroupName = "Group: " + eventGroup;
+        String lateTimeName = "Late Time: " + lateTime;
+        String attendees = "Attendees: " + numOfAttendees;
+
+        return eventName + next + eventGroupName + next + lateTimeName + next + attendees + next;
     }
 }
