0a6147dff44ba021016b508c7116b78609936a90
jacyerlsangre <sangrejacy@gmail.com>
2025-11-25 15:01:46 +0800

    Updated the CSVManager class

--- src/CSVManager.java
+++ src/CSVManager.java
@@ -1,17 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.*;
 import java.util.*;
 
 public class CSVManager {
     private Event event;
     private List<Person> listOfAttendees;
     private String lateTime;
-    private String pahtName;
+    private String pathName;
 
-    public CSVManager(){
-
+    public CSVManager(Event event){
+        this.event = event;
+        listOfAttendees = new ArrayList<>(event.getListOfAttendees());
+        lateTime = event.getLateTime();
+        pathName = event.getPathName();
     }
 
+    //Not sure on what this method should specifically return
     public String createCSVFile(){
-        return null;
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(pathName))){
+            for(Person p : listOfAttendees){
+                bw.write(p.toString());
+                bw.newLine();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return pathName;
     }
 
 
